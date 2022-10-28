9e833d530c47dd9afb525c33f861e9bd90e09b66
Alaa Aldandachi <58466561+AlaaDan@users.noreply.github.com>
2022-10-28 14:49:47 +0200

    title finder and letter frequency

--- Exe group/ObjectDictionary/index.js
+++ Exe group/ObjectDictionary/index.js
@@ -4,3 +4,57 @@ let book = {
     pagesNumber: 2000
 }
 
+let listOfObjects = [
+    {
+        title: "Game Of Thrones",
+        author: "Martin RR",
+        pagesNumber: 2000
+    },
+    {
+        title: "House of dragons",
+        author: "Martin RR",
+        pagesNumber: 2100
+    },
+    {
+        title: "Jack Sparrow",
+        author: "Martin",
+        pagesNumber: 2200
+    },
+
+]
+
+
+function titleFinder(listOfObjects, searchTitle){
+    for (const objects of listOfObjects) {
+        if(objects.title === searchTitle){
+            return objects
+        }
+        else{
+            return null
+        }
+
+        
+    }
+}
+
+//console.log(titleFinder(listOfObjects, "Game Of Thrones"));
+
+
+function letterFrequencey(word){
+    let wordObj = {};
+
+    for (let i = 0; i <= word.length; i++) {
+        let count  = 0
+        for (letter of word){
+            if (letter === word[i]){
+                count++
+                wordObj[letter] = count;
+            }
+        }    
+    };
+
+    return wordObj
+}
+
+//console.log(letterFrequencey("ni talar bra latin"));
+
